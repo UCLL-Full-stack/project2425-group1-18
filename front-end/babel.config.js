5da1dda9fcc832ba05a2791bdb6271929815237b
SzymonPartyka <145974498+SzymonPartyka@users.noreply.github.com>
2024-12-20 18:40:26 +0100

    test front-end

---
+++ front-end/babel.config.js
@@ -0,0 +1,8 @@
+// babel.config.js
+module.exports = {
+    presets: [
+      '@babel/preset-env',   // Allows Babel to transform modern JavaScript
+      '@babel/preset-react', // Transforms JSX syntax
+    ],
+  };
+  
